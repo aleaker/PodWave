8da88aa7fc12f6f95e2e210293cab6d34366bd5f
aleaker <alejoakerfeld@gmail.com>
2023-04-08 00:47:52 -0300

    add basic Header and GeneralLayout components

--- src/components/layouts/Header/Header.styles.js
+++ src/components/layouts/Header/Header.styles.js
@@ -1,6 +1,12 @@
 import styled, { keyframes } from "styled-components"
 
-export const HeaderContainer = styled.div``
+export const HeaderContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	border-bottom: 1px solid;
+	padding: 20px 5px 10px 5px;
+	margin-bottom: 20px;
+`
 
 export const Title = styled.span``
 
@@ -17,10 +23,10 @@ const LoaderAnimation = keyframes`
 `
 
 export const Loader = styled.div`
-	width: 20px;
-	height: 20px;
+	width: 15px;
+	height: 15px;
 	border-radius: 50%;
 	background-color: blue;
-	box-shadow: 0 0 20px #fff;
+	box-shadow: 0 0 15px #fff;
 	animation: ${LoaderAnimation} 1.5s ease-in-out infinite;
 `
