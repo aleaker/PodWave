069425c69f96e4224681fe2f6043c288cc9aa115
aleaker <alejoakerfeld@gmail.com>
2023-04-07 23:55:24 -0300

    install PropTypes and implement checks in Header component

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,12 +1,22 @@
+import PropTypes from "prop-types"
+
 import { HeaderContainer, Title, Loader } from "./Header.styles"
 
-const Header = loading => {
+const Header = ({ loading }) => {
 	return (
 		<HeaderContainer>
 			<Title> Podcaster </Title>
 			{loading ? <Loader /> : null}
 		</HeaderContainer>
 	)
+}
+
+Header.defaultProps = {
+	loading: false,
+}
+
+Header.propTypes = {
+	loading: PropTypes.bool,
 }
 
 export default Header
