48fff8d55953b2d465a7a928634fb6e063cbff2c
aleaker <alejoakerfeld@gmail.com>
2023-04-08 11:49:03 -0300

    remove loading prop

--- src/components/landing/SearchBar/SearchBar.jsx
+++ src/components/landing/SearchBar/SearchBar.jsx
@@ -2,23 +2,21 @@ import PropTypes from "prop-types"
 
 import { SearchBarContainer, AmountDisplay, Input } from "./SearchBar.styles"
 
-const SearchBar = ({ loading, amount, handleSearch }) => {
+const SearchBar = ({ amount, handleSearch }) => {
 	return (
 		<SearchBarContainer>
-			<AmountDisplay> {loading ? amount : "loading"} </AmountDisplay>
+			<AmountDisplay> {amount} </AmountDisplay>
 			<Input placeholder="Filter podcasts..." onChange={handleSearch} />
 		</SearchBarContainer>
 	)
 }
 
 SearchBar.defaultProps = {
-	loading: false,
 	amount: null,
 }
 
 SearchBar.propTypes = {
 	handleSearch: PropTypes.func.isRequired,
-	loading: PropTypes.bool,
 	amount: PropTypes.number,
 }
 
