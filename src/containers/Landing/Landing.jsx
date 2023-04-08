545e6659e46bca07bd8a16e4ef27542c4f8267e3
aleaker <alejoakerfeld@gmail.com>
2023-04-07 23:07:54 -0300

    add basic Landing containers

--- src/containers/Landing/Landing.jsx
+++ src/containers/Landing/Landing.jsx
@@ -1,5 +1,12 @@
+import { LandingContainer, SearchContainer, CardsContainer } from "./Landing.styles"
+
 const Landing = () => {
-	return <p>Landing</p>
+	return (
+		<LandingContainer>
+			<SearchContainer> Search </SearchContainer>
+			<CardsContainer> Cards </CardsContainer>
+		</LandingContainer>
+	)
 }
 
 export default Landing
