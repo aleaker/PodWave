b8d1eb984778f94389994621aa9516f81dc84003
aleaker <alejoakerfeld@gmail.com>
2023-04-09 18:22:39 -0300

    render PodcastDetails in Podcast container using hardcoded data

--- src/containers/Podcast/Podcast.jsx
+++ src/containers/Podcast/Podcast.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router-dom"
 
+import PodcastDetails from "components/podcast/PodcastDetails/PodcastDetails"
+
 import { PodcastContainer } from "./Podcast.styles"
 
 const Podcast = () => {
 	return (
 		<PodcastContainer>
+			<PodcastDetails
+				imgSrc="https://is3-ssl.mzstatic.com/image/thumb/Podcasts116/v4/30/1c/1f/301c1f05-639c-bb22-cfdd-4c71aca5761e/mza_3479668065976336868.png/170x170bb.png"
+				title="Podcast title"
+				author="Author of podcast"
+				description="Podcast text description"
+			/>
 			<Outlet />
 		</PodcastContainer>
 	)
