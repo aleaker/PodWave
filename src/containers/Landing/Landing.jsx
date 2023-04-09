e5abb96b1db8234408586f7739eb5b33d361065c
aleaker <alejoakerfeld@gmail.com>
2023-04-09 00:59:30 -0300

    integrate Landing with useDataResolver hook

--- src/containers/Landing/Landing.jsx
+++ src/containers/Landing/Landing.jsx
@@ -1,87 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import SearchBar from "components/landing/SearchBar/SearchBar"
 import PodcastCard from "components/landing/PodcastCard/PodcastCard"
+
+import useDataResolver from "hooks/useDataResolver"
 
 import { debounce } from "util/helpers"
 
 import { LandingContainer, SearchContainer, CardsContainer } from "./Landing.styles"
 
-const MOCK_DATA = [
-	{
-		id: "asd123",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd124",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd125",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd126",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd127",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd128",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd129",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd130",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd131",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-	{
-		id: "asd132",
-		title: "Podcast Name",
-		author: "Podcast Author",
-		imgSrc:
-			"https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png",
-	},
-]
-
 const Landing = () => {
 	const [, setSearchValue] = useState("")
+
+	const { data, getData } = useDataResolver("list")
 
 	const handleSearch = debounce(e => {
 		if (e.target.value) {
@@ -90,17 +21,23 @@ const Landing = () => {
 	})
 
 	const renderCards = () => {
-		return MOCK_DATA.map(({ id, title, author, imgSrc }) => (
+		return data.list.map(({ id, title, author, imgSrc }) => (
 			<PodcastCard key={id} title={title} author={author} imgSrc={imgSrc} />
 		))
 	}
+
+	useEffect(() => {
+		if (!data) {
+			getData("podcasts")
+		}
+	}, [])
 
 	return (
 		<LandingContainer>
 			<SearchContainer>
 				<SearchBar handleSearch={handleSearch} />
 			</SearchContainer>
-			<CardsContainer>{renderCards()}</CardsContainer>
+			<CardsContainer>{data && renderCards()}</CardsContainer>
 		</LandingContainer>
 	)
 }
