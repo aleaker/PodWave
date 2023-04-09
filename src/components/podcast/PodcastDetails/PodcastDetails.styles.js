9e8a1b249b5213cb25226926eacdb23065088b89
aleaker <alejoakerfeld@gmail.com>
2023-04-09 18:20:00 -0300

    add basic PodcastDetails ui

---
+++ src/components/podcast/PodcastDetails/PodcastDetails.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+import DisplayCard from "components/general/DisplayCard/DisplayCard"
+
+export const StyledDisplayCard = styled(DisplayCard)``
+
+export const PodcastLogo = styled.img``
+
+export const TextContainer = styled.div``
+
+export const Title = styled.span``
+
+export const Author = styled.span``
+
+export const StyledText = styled.span``
+
+export const Description = styled.p``
