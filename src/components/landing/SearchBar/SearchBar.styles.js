aacff506a8ae3b560b6a546b3603e082baa3e440
aleaker <alejoakerfeld@gmail.com>
2023-04-09 13:58:00 -0300

    adjust SearchBar styles

--- src/components/landing/SearchBar/SearchBar.styles.js
+++ src/components/landing/SearchBar/SearchBar.styles.js
@@ -1,5 +1,7 @@
 import styled from "styled-components"
 
+import { battleshipGray, blue, powder } from "theme/colors"
+
 export const SearchBarContainer = styled.div`
 	display: flex;
 `
@@ -9,9 +11,14 @@ export const AmountDisplay = styled.span`
 	justify-content: center;
 	padding: auto;
 	margin-right: 10px;
-	background: blue;
+	background: ${blue};
+	color: ${powder};
+	font-weight: bold;
 	border-radius: 20px;
 	min-width: 40px;
 `
 
-export const Input = styled.input``
+export const Input = styled.input`
+	border: 1px solid ${battleshipGray};
+	border-radius: 5px;
+`
