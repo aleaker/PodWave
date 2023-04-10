9e73978d6df8f5b467cd504f334ee1d7efc4c3a3
aleaker <alejoakerfeld@gmail.com>
2023-04-09 21:46:10 -0300

    add EpisodesTable component with hardcoded data

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -4,6 +4,7 @@ import Landing from "containers/Landing/Landing"
 
 import Podcast from "containers/Podcast/Podcast"
 import GeneralLayout from "components/layouts/GeneralLayout/GeneralLayout"
+import EpisodesTable from "components/podcast/EpisodesTable/EpisodesTable"
 
 import ROUTES from "constants/routes"
 
@@ -14,7 +15,7 @@ const App = () => {
 				<Routes>
 					<Route path={ROUTES.LANDING} element={<Landing />} />
 					<Route path={`${ROUTES.PODCAST}/:podcastId`} element={<Podcast />}>
-						<Route index element={<div>Podcast table</div>} />
+						<Route index element={<EpisodesTable />} />
 						<Route
 							path={`${ROUTES.PODCAST}/:podcastId/${ROUTES.EPISODE}/:episodeId`}
 							element={<div>Episode details</div>}
