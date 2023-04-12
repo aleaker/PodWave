9849a587fe36858b02543556ef2e170b7ad0aa48
aleaker <alejoakerfeld@gmail.com>
2023-04-12 02:53:19 -0300

    delete useDataResolver hook and refactor Landing to use deffered data

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from "react-router-dom"
 
-import Landing from "containers/Landing/Landing"
+import Landing, { landingLoader } from "containers/Landing/Landing"
 
 import Podcast, { podcastLoader } from "containers/Podcast/Podcast"
 import GeneralLayout from "components/layouts/GeneralLayout/GeneralLayout"
@@ -11,7 +11,7 @@ import ROUTES from "constants/routes"
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route element={<GeneralLayout />}>
-			<Route path={ROUTES.LANDING} element={<Landing />} />
+			<Route path={ROUTES.LANDING} element={<Landing />} loader={landingLoader} />
 			<Route
 				path={`${ROUTES.PODCAST}/:podcastId`}
 				element={<Podcast />}
