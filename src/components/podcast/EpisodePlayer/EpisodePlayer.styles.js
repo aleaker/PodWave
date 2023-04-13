d4f07d6bf6d485e8220f115e681bc36e8cf69cfd
aleaker <alejoakerfeld@gmail.com>
2023-04-13 00:35:33 -0300

    adjust EpisodePlayer DisplayCard styles

--- src/components/podcast/EpisodePlayer/EpisodePlayer.styles.js
+++ src/components/podcast/EpisodePlayer/EpisodePlayer.styles.js
@@ -4,12 +4,18 @@ import DisplayCard from "components/general/DisplayCard/DisplayCard"
 
 import { battleshipGray, onyx } from "theme/colors"
 
-export const StyledDisplayCard = styled(DisplayCard)``
+export const StyledDisplayCard = styled(DisplayCard)`
+	width: 90%;
+	height: fit-content;
+`
 
 export const StyledAudio = styled.audio``
 
 export const Title = styled.span`
 	color: ${onyx};
+	font-weight: bold;
+	font-size: 25px;
+	margin-top: auto;
 `
 
 export const Description = styled.p`
